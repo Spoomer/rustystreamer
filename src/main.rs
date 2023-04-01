d1f3b5cd4cb8fea8c8e46c2411452f5f58e1e8dc
Spoomer <renerahnfeld@gmail.com>
2023-04-02 00:47:16 +0200

    separate video page from index

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use actix_files::NamedFile;
 use actix_web::{
     get,
     http::header::{self, ContentType},
-    web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result,
+    web, App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 use std::{
     io::{BufReader, Read, Seek, SeekFrom},
@@ -10,27 +9,50 @@ use std::{
 };
 use videoflix::RangeHeader;
 
+const VIDEO_PATH: &str = "video";
 #[get("/")]
-async fn index() -> Result<NamedFile> {
+async fn index() -> impl Responder {
     let path: PathBuf = "./index.html".parse().unwrap();
-    Ok(NamedFile::open(path)?)
+    let mut file = std::fs::read_to_string(path).unwrap();
+    file = file
+        .replace("{title}", "Testvideo")
+        .replace("{videoId}", "Testvideo");
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(file)
 }
-
-#[get("/{name}")]
-async fn hello(name: web::Path<String>, request: HttpRequest) -> impl Responder {
-    if name.to_string() == "test" {
+#[get("/video/{name}")]
+async fn video_page(name: web::Path<String>) -> impl Responder {
+    if name.to_string() == "Testvideo" {
+        let path: PathBuf = "./video.html".parse().unwrap();
+        let mut file = std::fs::read_to_string(path).unwrap();
+        file = file
+            .replace("{title}", "Testvideo")
+            .replace("{videoId}", "test.mp4");
+        return HttpResponse::Ok()
+            .content_type(ContentType::html())
+            .body(file);
+    }
+    HttpResponse::NotFound().finish()
+}
+#[get("/video-resource/{name}")]
+async fn load_video(name: web::Path<String>, request: HttpRequest) -> impl Responder {
+    if name.to_string() == "test.mp4" {
         let header_map = request.headers();
         if !header_map.contains_key(header::RANGE) {
             return HttpResponse::BadRequest().finish();
         }
-        let size = std::fs::metadata("test.mp4").unwrap().len();
+        let mut path = PathBuf::new();
+        path.push(VIDEO_PATH);
+        path.push("test.mp4");
+        let size = std::fs::metadata(&path).unwrap().len();
         let range = header_map.get(header::RANGE).unwrap();
         let range_header = RangeHeader::parse(range, size).unwrap();
         if !(range_header.unit == "bytes") {
             return HttpResponse::BadRequest().finish();
         }
         let content_length = range_header.end - range_header.start + 1;
-        let file = std::fs::File::open("test.mp4").unwrap();
+        let file = std::fs::File::open(path).unwrap();
         let mut buf_reader = BufReader::new(file);
         let _position = buf_reader
             .seek(SeekFrom::Start(range_header.start))
@@ -50,7 +72,8 @@ async fn hello(name: web::Path<String>, request: HttpRequest) -> impl Responder 
                 "Content-Range",
                 format!("bytes {}-{}/{}", range_header.start, range_header.end, size),
             ))
-            .append_header(("Accept-Range", "bytes")).body(content);
+            .append_header(("Accept-Range", "bytes"))
+            .body(content);
         return response;
     }
     HttpResponse::NotFound().finish()
@@ -58,8 +81,13 @@ async fn hello(name: web::Path<String>, request: HttpRequest) -> impl Responder 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(video_page)
+            .service(load_video)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
