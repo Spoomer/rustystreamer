0fb2ad45b8b030537f984cc237e114d667825371
Spoomer <renerahnfeld@gmail.com>
2023-07-30 14:45:35 +0200

    add tailwind for production

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.html", "./js/**/*.js", "./src/consts.rs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
