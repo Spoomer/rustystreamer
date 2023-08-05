65bd51fcab10c2616789a9b5a138ad76541ee7fa
Spoomer <renerahnfeld@gmail.com>
2023-08-05 17:27:12 +0200

    bump version

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,4 @@
-pub const CHUNK_SIZE: usize = usize::pow(2, 20); // 1MB;
+pub const CHUNK_SIZE: usize = usize::pow(2, 20); // 1MB
 pub const VIEW_PATH: &str = "views";
 pub const VIDEO_LIST_HTML: &str = r#"<a href="/video/{videoId}" class="video-list-item"><div class="max-w-sm rounded overflow-hidden"><img src="{thumbnailLink}" class="w-full" alt="cover-picture"><h5 class="font-medium text-xl mb-2">{title}</h5></div></a>"#;
 pub const VIDEO_INDEX_PATH: &str = "videoIndex.json";
